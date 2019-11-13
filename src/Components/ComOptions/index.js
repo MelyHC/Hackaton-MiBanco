187056ac94a8875241b2cf73e5d641ec6c851f2a
GitKatKode <sabrinaacm@gmail.com>
2019-11-13 07:29:24 -0500

    inicio de estilos

--- src/Components/ComOptions/index.js
+++ src/Components/ComOptions/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Button} from 'react-bootstrap';
-
+import Header from '../Header';
 
 class Options extends Component {
 
@@ -12,24 +12,33 @@ class Options extends Component {
     render() {
         return(
             <React.Fragment>
-                 <Button variant="success"
-                    size="lg"
-                    onClick={this.handleClick}
-                    type="submit"  
-                    value="/register"
-                    >Registro de Convenio/ TECA</Button>
-                    <Button variant="success"
-                    size="lg"
-                    onClick={this.handleClick}
-                    type="submit"  
-                    value="/resumen"
-                    >Módulo de Resúmen</Button>
-                    <Button variant="success"
-                    size="lg"
-                    onClick={this.handleClick}
-                    type="submit"  
-                    value="/obs"
-                    >Observaciones de Asignación</Button>
+                <div className="cont-options">
+                <Header></Header>
+               <div className="button-options">
+                <div className="cont-btn-asig">
+                <span className="obs-asig" onClick={this.handleClick}>OBSERVACIONES DE ASIGNACIÓN</span>
+                <figure className="btn-obs-asig"></figure>
+                </div>
+               {/* <Button variant="success"
+                  size="lg"
+                  onClick={this.handleClick}
+                  type="submit"  
+                  value="/register"
+                  >Registro de Convenio/ TECA</Button>
+                  <Button variant="success"
+                  size="lg"
+                  onClick={this.handleClick}
+                  type="submit"  
+                  value="/resumen"
+                  >Módulo de Resúmen</Button>
+                  <Button variant="success"
+                  size="lg"
+                  onClick={this.handleClick}
+                  type="submit"  
+                  value="/obs"
+                  >Observaciones de Asignación</Button> */}
+               </div>
+                </div>
             </React.Fragment>
         )
     }
