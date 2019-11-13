dad3464c0d5739363b54576af52296cbc3647eb5
GitKatKode <sabrinaacm@gmail.com>
2019-11-13 01:28:04 -0500

    1:28

--- src/Components/ObsAsig/index.js
+++ src/Components/ObsAsig/index.js
@@ -3,32 +3,56 @@ import { range } from 'lodash';
 import ReactDataGrid from 'react-data-grid'; // Tested with v5.0.4, earlier versions MAY NOT HAVE cellRangeSelection
 import { logout, db } from '../../Firebase';
 
+
 const columns = [
-  { key: 'COD_CLI', name: 'COD_CLI', editable: true },
-  { key: 'FEC_REG', name: 'FEC_REG', editable: true },
-  { key: 'FEC_ULT_MOD', name: 'FEC_ULT_MOD', editable: true },
-  { key: 'PERIODO', name: 'PERIODO', editable: true },
-  { key: 'SUPERVISOR', name: 'SUPERVISOR', editable: true },
-  { key: 'SUSTENTO', name: 'SUSTENTO', editable: true },
-  { key: 'USU_FIN', name: 'USU_FIN', editable: true },
-  { key: 'USU_NT_MOD', name: 'USU_NT_MOD', editable: true },
-  { key: 'USU_NT_REG', name: 'USU_NT_REG', editable: true },
+  { key: 'CODIGO_CLIENTE', name: 'CODIGO_CLIENTE', editable: true },
+  { key: 'EDR_ANTERIOR', name: 'EDR_ANTERIOR' },
+  { key: 'EDR_ASIGNADO', name: 'EDR_ASIGNADO' },
+  { key: 'HERRAMIENTA', name: 'HERRAMIENTA' },
+  { key: 'EDR_SUPER', name: 'EDR_SUPER', editable: true },
+  { key: 'JUSTIFICACION', name: 'JUSTIFICACION', editable: true },
 ];
 
-// const initialRows = Array.from(Array(1000).keys(), (_, x) => (
-//   { id: x, title: x * 2, count: x * 3, sarah: x * 4, jessica: x * 5 }
-// ));
 
 const defaultParsePaste = str => (
   str.split(/\r\n|\n|\r/)
     .map(row => row.split('\t'))
 );
 
+let row0 = {
+  "CODIGO_CLIENTE": null,
+  "TIPO_DOC": "",
+  "NRO_DOC": null,
+  "NOMBRE_CLIENTE": "",
+  "MONTO_PRESTAMO": null,
+  "CODIGO_PRESTAMO": null,
+  "DIAS_MORA": null,
+  "MONEDA": "",
+  "CANT_CLIEN": null,
+  "AGENCIA": "",
+  "ZONA": "",
+  "TERRITORIAL": "",
+  "SUPERVISOR": "",
+  "EJECUTIVO": "",
+  "HERRAMIENTA": "",
+  "ESTADO": "",
+  "EDR_ANTERIOR": "",
+  "EDR ASIGNADO": "",
+  "EDR SUPER": "",
+  "MONTO_CUOTA": null,
+  "NRO CUOTA": null,
+  "FECH_INIC": "",
+  "FECH_VENCI": "",
+  "MODELO": "",
+  "SALDO_AGENCIA": null,
+  "JUSTIFICACION": ""
+}
+
 class MyDataGrid extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rows: [],
+      rows: [row0],
       topLeft: {},
       botRight: {},
     };
@@ -36,20 +60,6 @@ class MyDataGrid extends Component {
     // Copy paste event handler
     document.addEventListener('copy', this.handleCopy);
     document.addEventListener('paste', this.handlePaste);
-  }
-
-  componentWillMount() {
-    const currentRows = [];
-    db.collection("ObsAsig").get().then((doc) => {
-      doc.forEach(obs => {
-        currentRows.push(obs.data());
-      }
-      )
-      this.setState({ rows: currentRows });
-    }).catch((error) => {
-      console.log("ERROR:", error);
-    }
-    );
   }
 
 
@@ -76,7 +86,7 @@ class MyDataGrid extends Component {
 
   rowGetter = (i) => {
     const { rows } = this.state;
-    return rows[i];
+    return rows[i]
   }
 
   updateRows = (startIdx, newRows) => {
@@ -134,10 +144,45 @@ class MyDataGrid extends Component {
 
     this.updateRows(topLeft.rowIdx, newRows);
   }
+  // componentWillMount() {
+  //   const currentRows = [];
+  //   db.collection("DataBase").get().then((doc) => {
+  //     doc.forEach(obs => {
+  //       currentRows.push(obs.data());
+  //       console.log(obs.data());
+
+  //     }
+  //     )
+  //     this.setState({ rows: currentRows });
+  //   }).catch((error) => {
+  //     console.log("ERROR:", error);
+  //   }
+  //   );
+  // }
+  getFirebaseData = (client) => {
+    let currentRows= [];
+    db.collection("DataBase").where("CODIGO_CLIENTE", '==', client).get().then((doc) => {
+      doc.forEach(obs => {
+        currentRows.push(obs.data())
+      }
+      )
+
+      this.setState({ rows: currentRows });
+    }).catch((error) => {
+      console.log("ERROR:", error);
+    }
+    );
+  }
 
   onGridRowsUpdated = ({ fromRow, toRow, updated, action }) => {
     console.debug('onGridRowsUpdated!', action);
     console.debug('updated', updated);
+    if (updated.CODIGO_CLIENTE) {
+
+      this.getFirebaseData(parseInt(updated.CODIGO_CLIENTE))
+
+    }
+
     if (action !== 'COPY_PASTE') {
       this.setState((state) => {
         const rows = state.rows.slice();
@@ -168,7 +213,8 @@ class MyDataGrid extends Component {
       <div>
         <ReactDataGrid
           columns={columns}
-          rowGetter={i => rows[i]}
+          rowGetter={i => rows[i]
+          }
           rowsCount={rows.length}
           onGridRowsUpdated={this.onGridRowsUpdated}
           enableCellSelect
